20d8c4cd23e79e7ec1a23e9603bf9b5290d75d2d
Matheus Ashton Silva <matheusashton@gmail.com>
2018-03-27 00:06:14 -0300

    project initial structure

---
+++ main.py
@@ -0,0 +1,8 @@
+from flask.helpers import get_debug_flag
+
+from api.app import create_app
+from api.settings import DevConfig, ProdConfig
+
+CONFIG = DevConfig if get_debug_flag() else ProdConfig
+
+app = create_app(CONFIG)
